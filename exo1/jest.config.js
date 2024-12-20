e23734e6cf5adcfb08c8472df5d54e6d916fecbb
mehdihnc <mehdiharchenif@gmail.com>
2024-12-20 10:05:00 +0100

    Reorganisation de la structure des fichiers pour exo1

---
+++ exo1/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': ['babel-jest', { configFile: './.babelrc' }]
+  },
+  testMatch: ['**/__tests__/**/*.test.js'],
+  verbose: true,
+  rootDir: 'src'
+}; 
